e3add97dea43301679e305503561a0b2be08e00f
prerona <preronakvch@gmail.com>
2022-08-16 02:47:23 +0530

    mongosessionassignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,29 @@
-const UserModel= require("../models/userModel")
+const bookModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
+const createaddbook= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
+    let savedData= await bookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getbookData= async function (req, res) {
+    let allbooks= await bookModel.find()
+    res.send({msg: allbooks})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+    module.exports.createaddbook= createaddbook
+     module.exports.getbookData= getbookData
+
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
+
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://preronaDatabase:RX1UhhpAWmeEPPyB@cluster0.cv0moqj.mongodb.net/preronaplutonium", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
